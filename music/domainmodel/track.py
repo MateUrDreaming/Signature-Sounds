73df4d0f232e8a066c1c749ea6c9d6e84d3cdc9b
pkas <pkas830@aucklanduni.ac.nz>
2022-08-31 10:11:29 +1200

    Fixed compatiability with python 3.8.10

--- music/domainmodel/track.py
+++ music/domainmodel/track.py
@@ -14,10 +14,10 @@ class Track:
             self.__title = track_title.strip()
 
         self.__artist = None
-        self.__album: Album | None = None
-        self.__track_url: str | None = None
+        self.__album: Album  = None
+        self.__track_url: str  = None
         # duration in seconds
-        self.__track_duration: int | None = None
+        self.__track_duration: int  = None
         self.__genres: list = []
 
     @property
