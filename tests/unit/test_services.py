56dbe1e6c7473bf4cbc404808d5c0d5441c7e77d
harlean <harlean14@gmail.com>
2022-09-23 19:52:35 +1200

    added tests for user services

--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -154,3 +154,132 @@ def test_playlists(in_memory_repo):
     tracks_services.remove_from_playlist(in_memory_repo, 2, user.playlist[0].list_id)
 
     assert user.playlist[0].size() == 3
+
+
+''' Testing user services. '''
+
+def test_getting_all_liked_tracks(in_memory_repo):
+    new_user_name = 'jnlennon'
+    new_password = 'abcd1A23'
+
+    auth_services.add_user(new_user_name, new_password, in_memory_repo)
+    user = auth_services.get_user_object(new_user_name, in_memory_repo)
+    assert user.user_name == 'jnlennon'
+
+    tracks = tracks_services.get_track_ids_for_query(query="AWOL", type="artists", repo=in_memory_repo)
+
+    for track_id in tracks: 
+        track = tracks_services.get_track_object_by_id(track_id, in_memory_repo)
+
+        tracks_services.add_track_to_likes(in_memory_repo, user, track)
+    assert len(user_services.get_all_liked_tracks(user, in_memory_repo)) == 4
+
+def test_adding_a_playlist(in_memory_repo):
+    new_user_name = 'jnlennon'
+    new_password = 'abcd1A23'
+
+    auth_services.add_user(new_user_name, new_password, in_memory_repo)
+    user = auth_services.get_user_object(new_user_name, in_memory_repo)
+    assert user.user_name == 'jnlennon'
+
+    title = 'A Test Playlist'
+    user_services.add_playlist(in_memory_repo, title, user)
+    assert len(user_services.get_user_playlists(in_memory_repo, user)) == 1
+
+def test_getting_playlist_by_id(in_memory_repo):
+    new_user_name = 'jnlennon'
+    new_password = 'abcd1A23'
+
+    auth_services.add_user(new_user_name, new_password, in_memory_repo)
+    user = auth_services.get_user_object(new_user_name, in_memory_repo)
+    assert user.user_name == 'jnlennon'
+
+    title = 'A Test Playlist'
+    user_services.add_playlist(in_memory_repo, title, user)
+
+    playlist = user_services.get_playlist_by_id(in_memory_repo, 1)
+    assert playlist.user == 'jnlennon'
+
+def test_getting_user_playlists(in_memory_repo):
+    new_user_name = 'jnlennon'
+    new_password = 'abcd1A23'
+
+    auth_services.add_user(new_user_name, new_password, in_memory_repo)
+    user = auth_services.get_user_object(new_user_name, in_memory_repo)
+    assert user.user_name == 'jnlennon'
+
+    title = 'A Test Playlist'
+    user_services.add_playlist(in_memory_repo, title, user)
+    title2 = 'A Test Playlist 2'
+    user_services.add_playlist(in_memory_repo, title2, user)
+
+    assert len(user_services.get_user_playlists(in_memory_repo, user)) == 2
+
+def test_getting_user_reviews(in_memory_repo):
+    new_user_name = 'jnlennon'
+    new_password = 'abcd1A23'
+
+    auth_services.add_user(new_user_name, new_password, in_memory_repo)
+    user = auth_services.get_user_object(new_user_name, in_memory_repo)
+    assert user.user_name == 'jnlennon'
+
+    assert len(user_services.get_user_reviews(user, in_memory_repo)) == 0
+
+def test_removing_playlist(in_memory_repo):
+    new_user_name = 'jnlennon'
+    new_password = 'abcd1A23'
+
+    auth_services.add_user(new_user_name, new_password, in_memory_repo)
+    user = auth_services.get_user_object(new_user_name, in_memory_repo)
+    assert user.user_name == 'jnlennon'
+
+    title = 'A Test Playlist'
+    user_services.add_playlist(in_memory_repo, title, user)
+
+    user_services.remove_playlist(in_memory_repo, user, 1)
+    assert len(user_services.get_user_playlists(in_memory_repo, user)) == 0
+
+def test_changing_playlist_visibility(in_memory_repo):
+    new_user_name = 'jnlennon'
+    new_password = 'abcd1A23'
+    auth_services.add_user(new_user_name, new_password, in_memory_repo)
+    user = auth_services.get_user_object(new_user_name, in_memory_repo)
+    
+    title = 'A Test Playlist'
+    user_services.add_playlist(in_memory_repo, title, user)
+
+    user_services.change_visibility(in_memory_repo, user, 1)
+    assert user.playlist[0].is_public == True
+
+def test_getting_all_playlists(in_memory_repo):
+    new_user_name = 'jnlennon'
+    new_password = 'abcd1A23'
+    auth_services.add_user(new_user_name, new_password, in_memory_repo)
+    user = auth_services.get_user_object(new_user_name, in_memory_repo)
+    
+    title = 'A Test Playlist'
+    user_services.add_playlist(in_memory_repo, title, user)
+
+    user_services.change_visibility(in_memory_repo, user, 1)
+    assert user.playlist[0].is_public == True
+    assert len(user_services.get_all_playlists(in_memory_repo)) == 1
+
+def test_adding_public_playlist(in_memory_repo):
+    new_user_name = 'jnlennon'
+    new_password = 'abcd1A23'
+    auth_services.add_user(new_user_name, new_password, in_memory_repo)
+    user = auth_services.get_user_object(new_user_name, in_memory_repo)
+    
+    title = 'A Test Playlist'
+    user_services.add_playlist(in_memory_repo, title, user)
+    user_services.change_visibility(in_memory_repo, user, 1)
+
+    new_user_name = 'pmccartney'
+    new_password = 'abcd1A23'
+
+    auth_services.add_user(new_user_name, new_password, in_memory_repo)
+    user2 = auth_services.get_user_object(new_user_name, in_memory_repo)
+    assert user2.user_name == 'pmccartney'
+
+    user_services.add_public_playlist(in_memory_repo, user2, 1)
+    assert len(user_services.get_user_playlists(in_memory_repo, user2)) == 1  
